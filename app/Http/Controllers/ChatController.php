13ccfa80fb577edbf9357cacbfca9f94c803a9e5
秋元崇利 <takatoshi.akimoto@rakus-pertners.co.jp>
2022-10-31 09:21:30 +0900

    修正済み エラー解消（->）を（=>）に変更

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -15,12 +15,15 @@ class ChatController extends Controller
      */
     public function index(Request $request)
     {
-        $user_identifier = $request->session()->get('user_identifier', Str::random(20));
-        session(['user_identifier' => $user_identifier]);       
+        //$user_identifier = $request->session()->get('user_identifier', Str::random(20));
+        //session(['user_identifier' => $user_identifier]);  
+        
+        //ユーザー識別子がなければランダムに生成してセッションに登録
+        if($request->session()->missing('user_identifier')){session(['user_identifier' => Str::ramdom(20)]);}
         
        //ユーザー名を変数に登録（デフォルト時：Guest）
        //$user_name = $request->session()->get('user_name', 'Guest');
-       if($request->session()->missing('user_name')){session(['user_name' => 'Guest']);}
+       if($request->session()->missing('user_name')){session(['user_name' => 'Guest']); }
 
         //データベースの件数を取得 
        $length = Chat::all()->count();
@@ -29,6 +32,7 @@ class ChatController extends Controller
        $display = 5;
 
       $chats = Chat::offset($length-$display)->limit($display)->get();
+      //チャットをビューに渡して表示
       return view('chat/index', compact('chats'));
 
     }
@@ -52,10 +56,7 @@ class ChatController extends Controller
     public function store(Request $request)
     {
         //ユーザー名を取得、セッションに登録
-        //sesssion(['user_name'->$request->user_name]);
-
-        //ユーザー識別子がなければランダムに生成してセッションに登録
-        if($request->session()->missing('user_indentifier')){session(['user_identifier' => Str::ramdom(20)]);}
+        session(['user_name'=>$request->user_name]);
 
         $chat = new Chat;
         $form = $request->all();
